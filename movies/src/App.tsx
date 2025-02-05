97becad87cceba181486944adde1634c311703a0
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-02-05 17:34:59 +0530

    initialize React app with Vite and TypeScript setup

---
+++ movies/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-sky-600">Movies</h1>
+      <h3>Movie Streaming Platform</h3>
+    </>
+  );
+}
+
+export default App;
