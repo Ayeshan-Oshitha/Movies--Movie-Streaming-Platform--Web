5fd602419afbd49613be0b308c0ac1d3bf41202d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-04-06 17:52:09 +0530

    Updated the heroSection-image path

--- movies/src/components/Hero.tsx
+++ movies/src/components/Hero.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import heroImage from "../assets/hero-image.png";
 
 const Hero = () => {
   return (
     <div className=" h-[600px] xl:h-[600px] 2xl:h-[650px] 3xl:h-[800px] w-full relative shadow-gray-500 shadow-xl ">
-      <div className="bg-[url(src/assets/hero-image.png)] bg-cover bg-center h-full w-full "></div>
+      <div className="bg-cover bg-center h-full w-full " style={{ backgroundImage: `url(${heroImage})` }}></div>
       <div className="absolute inset-1  flex flex-col gap-y-8 justify-center items-center   text-center ">
         <p className="text-7xl text-white font-bold">Discover New Worlds</p>
         <p className="text-3xl text-white ">Explore top picks and hidden gems in cinema</p>
